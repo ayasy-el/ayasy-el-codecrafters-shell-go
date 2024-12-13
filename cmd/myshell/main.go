61b1b2a1d32d2244fd7499930249de33de56079b
zerobytez <zerobytez@mail.com>
2024-12-13 23:26:54 +0700

    type

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -15,12 +16,20 @@ func main() {
 		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
 		cmd := strings.Fields(input)
 
+		commands := []string{"exit", "echo", "type"}
+
 		switch cmd[0] {
 		case "exit":
 			code, _ := strconv.Atoi(cmd[1])
 			os.Exit(code)
 		case "echo":
 			fmt.Fprintln(os.Stdout, strings.Join(cmd[1:], " "))
+		case "type":
+			if slices.Contains(commands, cmd[1]) {
+				fmt.Fprintf(os.Stdout, "%s is a shell builtin\n", cmd[1])
+			} else {
+				fmt.Fprintf(os.Stdout, "%s: not found\n", cmd[1])
+			}
 		default:
 			fmt.Fprintf(os.Stdout, "%s: command not found\n", cmd[0])
 		}
