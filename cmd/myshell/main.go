a34728fcf14f1668220aa2d8435bca1c1380ab15
zerobytez <zerobytez@mail.com>
2024-12-13 22:39:29 +0700

    exit 0

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,13 +4,20 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	for true {
 		fmt.Fprint(os.Stdout, "$ ")
 
-		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		fmt.Fprint(os.Stdout, input[:len(input)-1]+": command not found\n")	
+		cmd, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		cmd = strings.TrimSpace(cmd)
+		switch cmd {
+		case "exit 0":
+			os.Exit(0)
+		default:
+			fmt.Printf("%s: command not found\n", cmd)
+		}
 	}
 }
