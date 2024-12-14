78afb445e9c2c45dc46eced7dbe2369d5fc9d1fe
zerobytez <zerobytez@mail.com>
2024-12-14 13:12:26 +0700

    run a program

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"slices"
 	"strconv"
@@ -11,41 +12,69 @@ import (
 )
 
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	commands := []string{"exit", "echo", "type"}
+
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
+		if !scanner.Scan() {
+			break
+		}
 
-		input, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-		cmd := strings.Fields(input)
-		path := strings.Split(os.Getenv("PATH"), ":")
-		commands := []string{"exit", "echo", "type"}
+		cmd := strings.Fields(scanner.Text())
+		if len(cmd) == 0 {
+			continue
+		}
 
 		switch cmd[0] {
 		case "exit":
-			code, _ := strconv.Atoi(cmd[1])
+			code := 0
+			if len(cmd) > 1 {
+				code, _ = strconv.Atoi(cmd[1])
+			}
 			os.Exit(code)
 		case "echo":
-			fmt.Fprintln(os.Stdout, strings.Join(cmd[1:], " "))
+			fmt.Println(strings.Join(cmd[1:], " "))
 		case "type":
+			if len(cmd) < 2 {
+				fmt.Println("type: missing argument")
+				continue
+			}
+
 			if slices.Contains(commands, cmd[1]) {
-				fmt.Fprintf(os.Stdout, "%s is a shell builtin\n", cmd[1])
-				break
-			}
-
-			found := false
-			for _, p := range path {
-				fullpath := filepath.Join(p, cmd[1])
-				if _, err := os.Stat(fullpath); err == nil {
-					found = true
-					fmt.Printf("%s is %s\n", cmd[1], fullpath)
-					break
-				}
-			}
-
-			if !found {
-				fmt.Fprintf(os.Stdout, "%s: not found\n", cmd[1])
+				fmt.Printf("%s is a shell builtin\n", cmd[1])
+			} else if foundCommand := findCommand(cmd[1]); foundCommand != "" {
+				fmt.Printf("%s is %s\n", cmd[1], foundCommand)
+			} else {
+				fmt.Printf("%s: not found\n", cmd[1])
 			}
 		default:
-			fmt.Fprintf(os.Stdout, "%s: command not found\n", cmd[0])
+			if foundCommand := findCommand(cmd[0]); foundCommand != "" {
+				execCommand(foundCommand, cmd)
+			} else {
+				fmt.Printf("%s: command not found\n", cmd[0])
+			}
 		}
+	}
+}
+
+func findCommand(command string) string {
+	pathDirs := strings.Split(os.Getenv("PATH"), ":")
+	for _, dir := range pathDirs {
+		fullPath := filepath.Join(dir, command)
+		if _, err := os.Stat(fullPath); err == nil {
+			return fullPath
+		}
+	}
+	return ""
+}
+
+func execCommand(command string, args []string) {
+	cmd := exec.Command(command, args[1:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 	}
 }
